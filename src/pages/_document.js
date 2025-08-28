b801000e5b0a3d306f1dc272286cfd2ba156e7b2
Suyash Pradhan <pradhansuyash39@gmail.com>
2025-08-28 22:36:34 +0530

    feat: added gtag manager code

---
+++ src/pages/_document.js
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4VJVGGRD00"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4VJVGGRD00');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
